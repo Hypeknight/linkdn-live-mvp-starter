ef653fa4b45ec7866e72b8f215187f25af1cd502
Hypeknight <willie_gerald1@yahoo.com>
2026-03-13 07:46:05 -0500

    config changes

--- web/common/config.js
+++ web/common/config.js
@@ -1,6 +1,6 @@
 window.LINKDN_CONFIG = {
   API_BASE: "https://YOUR-RENDER-BACKEND.onrender.com",
   SUPABASE_URL: "https://fpqxlkflbhnlitnzcvos.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_PUBLISHABLE_KEY",
+  SUPABASE_ANON_KEY: "sb_publishable_3UboZTqhyKKwr5cpl4yoZQ_Yk6LEWRc",
   APP_NAME: "Linkd'N"
 };

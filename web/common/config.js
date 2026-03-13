3f68cfa1b9ff2b7f6d900f05a22926783ee8ba4f
Hypeknight <willie_gerald1@yahoo.com>
2026-03-12 23:12:04 -0500

    config change

--- web/common/config.js
+++ web/common/config.js
@@ -1,6 +1,6 @@
 window.LINKDN_CONFIG = {
   SUPABASE_URL: "https://fpqxlkflbhnlitnzcvos.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_3UboZTqhyKKwr5cpl4yoZQ_Yk6LEWRc",
-  API_BASE_URL: "http://localhost:8787/api",
+  API_BASE_URL: "https://linkdn-backend.onrender.com",
   LIVEKIT_WS_URL: "wss://linkdn-0caiylqd.livekit.cloud"
 };
